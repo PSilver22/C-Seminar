ed94c6ec0f9ba466025d67058f9afe878b1613bf
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-08-11 14:28:02 +0300

    Add files via upload

---
+++ Homework 1/Question 1/isQ.h
@@ -0,0 +1,5 @@
+#pragma once
+/// @brief Checks if the given character is the letter q/Q
+/// @param letter Letter to check.
+/// @return True if character is 'q'/'Q', otherwise false.
+_Bool isQ(char letter);
