352d662b77b3b565029cc4d652f0dfb0845d570b
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-08-11 14:36:33 +0300

    Update Question 7.c

--- Homework 1/Question 7/Question 7.c
+++ Homework 1/Question 7/Question 7.c
@@ -1,14 +1,6 @@
-/*
-=============================================
-Author: Pinny Silver
-Course: C Workshop
-Assignment: Homework 1, Question 7
-Description: Inputs a stream of numbers, terminated on 0, then prints the reverse of the numbers.
-=============================================
-*/
-
 #include <stdio.h>
 
+/// @brief Inputs a stream of numbers, terminated on 0, then prints the reverse of the numbers.
 int main()
 {
     int input;
