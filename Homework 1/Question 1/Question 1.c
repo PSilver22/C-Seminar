f4c8b1f01d4306c5911e63dfaa224fd6444f6b24
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-08-11 14:28:30 +0300

    Add files via upload

---
+++ Homework 1/Question 1/Question 1.c
@@ -0,0 +1,22 @@
+/// @dir .\
+/// @author Pinny Silver\n
+/// @date 8/11/21
+/// Course: C Workshop\n
+#include <stdio.h>
+#include "isQ.h"
+
+/// @brief Inputs then prints the inputted character. Terminated on 'q' or 'Q'.
+int main()
+{
+    char inputChar;
+
+    do
+    {
+        printf("Input a character: ");
+        scanf_s(" %c", &inputChar);
+
+        printf("Your character is: %c\n", inputChar);
+    } while (!isQ(inputChar));
+
+    return 0;
+}
