e1834ce40ebdcc7eb06aa63ace0806295b9422f1
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-08-25 14:13:05 +0300

    Minor fixes

--- Homework 5/Question 2/program.c
+++ Homework 5/Question 2/program.c
@@ -13,38 +13,40 @@ int main(void)
         Number num2;
         unsigned int choice;
 
-        printf("What would you like to do?\n1. add\n2. subtract\n3. multiply\n4. division\n5. exit\n");
+        printf("What would you like to do?\n1. add\n2. subtract\n3. multiply\n4. division\n5. exit\nchoice:");
         scanf_s("%u", &choice);
+
+        if (choice == 5)
+        {
+            running = 0;
+            break;
+        }
+
+        num2 = inputNumber();
 
         switch(choice)
         {
         case 1:
-            num2 = inputNumber();
             num1 = add(num1, num2);
             break;
 
         case 2:
-            num2 = inputNumber();
             num1 = add(num1, negative(num2));
             break;
 
         case 3:
-            num2 = inputNumber();
             num1 = multiply(num1, num2);
             break;
 
         case 4:
-            num2 = inputNumber();
             num1 = divide(num1, num2);
             break;
-
-        default:
-            running = 0;
         }
+
+        printNumber(num1);
     }
 
     // print out final result for all of the operations.
-    printNumber(num1);
     
     return 0;
 }
